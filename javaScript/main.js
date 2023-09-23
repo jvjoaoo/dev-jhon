79013b7b377eda45d71b72709d721d19a7ec873c
jvjoaoo <jvjoaoo.vitor@gmail.com>
2023-09-23 07:06:19 -0300

    Ajustes pontuais

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,30 +1,29 @@
-const header = document.querySelector("header");
+const header = document.querySelector("header")
 
-window.addEventListener("scroll", diminuiHeader);
+window.addEventListener("scroll", diminuiHeader)
 
-function diminuiHeader(){
-    if(window.pageYOffset > 0 && header.classList.contains("scrollmax")){
-        header.classList.remove("scrollmax");
-        header.classList.add("scrollmin");
-    } else if(window.pageYOffset == 0 && header.classList.contains("scrollmin")){
-        header.classList.remove("scrollmin");
-        header.classList.add("scrollmax");
-    }
+function diminuiHeader() {
+  if (window.pageYOffset > 0 && header.classList.contains("scrollmax")) {
+    header.classList.remove("scrollmax")
+    header.classList.add("scrollmin")
+  } else if (
+    window.pageYOffset == 0 &&
+    header.classList.contains("scrollmin")
+  ) {
+    header.classList.remove("scrollmin")
+    header.classList.add("scrollmax")
+  }
 }
-
 
 //Opções para mascarar a URL
 // Opção 01
 
-//document.getElementById("sobre").addEventListener("click", function(event) {
-//    event.preventDefault(); // Impede o comportamento padrão do link
+document.getElementById("sobre").addEventListener("click", function (event) {
+  event.preventDefault() // Impede o comportamento padrão do link
 
-    // Mascarar a URL (substitua a URL abaixo pela URL desejada)
-//    var newURL = "https://www.exemplo.com";
+  // Mascarar a URL (substitua a URL abaixo pela URL desejada)
+  var newURL = "https://jvjoaoo.github.io/dev-jhon/"
 
-    // Redirecionar para a URL mascarada
-//    window.location.href = newURL;
-//});
-
-
-
+  // Redirecionar para a URL mascarada
+  window.location.href = newURL
+})
